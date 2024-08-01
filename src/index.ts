598f341ee99ea4422b35628ddba812379a625272
Brett Beutell <brbeut@gmail.com>
2024-08-01 13:42:20 +0200

    Make basic hermes example work

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,24 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+  AI: Ai;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+    const messages = [
+      { role: "system", content: "You are a friendly assistant" },
+      {
+        role: "user",
+        content: "What is the origin of the phrase Hello, World",
+      },
+    ];
+    const response = await env.AI.run(
+      "@hf/nousresearch/hermes-2-pro-mistral-7b" as BaseAiTextGenerationModels,
+      { messages } as BaseAiTextGeneration["inputs"]
+    );
+
+    return Response.json(response);
 	},
 } satisfies ExportedHandler<Env>;
